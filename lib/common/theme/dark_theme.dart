d84e1a2d92efe47574b8e7c282f99ef0241c5f14
Muhamad Farhan <muhamadfarhan.inc@gmail.com>
2023-06-04 10:16:00 +0700

    Memperbaiki Bug Kecil

--- lib/common/theme/dark_theme.dart
+++ lib/common/theme/dark_theme.dart
@@ -6,7 +6,7 @@ import 'package:whatsapp_messenger/common/utils/coloors.dart';
 ThemeData darkTheme() {
   final ThemeData base = ThemeData.dark();
   return base.copyWith(
-    backgroundColor: Coloors.backgroundDark,
+    canvasColor: Coloors.backgroundDark,
     scaffoldBackgroundColor: Coloors.backgroundDark,
     extensions: [CustomThemeExtension.darkMode],
     appBarTheme: const AppBarTheme(
--- lib/common/theme/light_theme.dart
+++ lib/common/theme/light_theme.dart
@@ -7,7 +7,7 @@ import '../extension/custom_theme_extension.dart';
 ThemeData lightTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
-    backgroundColor: Coloors.backgroundLight,
+    canvasColor: Coloors.backgroundLight,
     scaffoldBackgroundColor: Coloors.backgroundLight,
     extensions: [CustomThemeExtension.lightMode],
     appBarTheme: const AppBarTheme(
